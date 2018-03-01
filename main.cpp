b00df557f9afdb0af381dc943cce4e47e2adc1f7
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-28 17:58:34 -0800

    Tests luck with choosing door

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int replay;
+  int won = 0;
+  cout<<"I wanna play a game. How many times would you like to play?";
+  cin>> replay;
+  
+  for (int i = 0; i < replay; i ++)
+  {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+  if (game.isWinner() == true)
+   {
+   won += 1;
+   }
+  }
+ 
+  cout<<"You have won the game "<<won<<" times.";
+  
   return 0;
 }
